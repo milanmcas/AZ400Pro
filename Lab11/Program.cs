befa9d11c4d7137f99f417002373a389808a9bbf
milan <milanmcas@gmail.com>
2025-08-17 21:30:53 +0530

    app config

--- Lab11/Program.cs
+++ Lab11/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.FeatureManagement;
+
 namespace Lab11
 {
     public class Program
@@ -7,7 +9,20 @@ namespace Lab11
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            const string ConnectionString = "Endpoint=https://appcs-53852658.azconfig.io;Id=pFK4;Secret=5JjBVCVF96ZTpmmkKewPL0LF0EBmsTMv3suvoWlRTVQ1wOgQ3ubuJQQJ99BHAC8vTIndyv2KAAACAZAC8YWL";
 
+            builder.Services.Configure<MyConfig>(builder.Configuration.GetSection("MyNiceConfig"));
+            builder.Configuration.AddAzureAppConfiguration(options =>
+            {
+                options.Connect(ConnectionString).ConfigureRefresh((refreshOptions) =>
+                {
+                    // indicates that all configuration should be refreshed when the given key has changed.
+                    refreshOptions.Register(key: "MyNiceConfig:PageSize", refreshAll: true).SetCacheExpiration(TimeSpan.FromSeconds(10));
+                    //refreshOptions.SetCacheExpiration(TimeSpan.FromSeconds(5));
+                }).UseFeatureFlags();
+            });
+            builder.Services.AddAzureAppConfiguration();
+            builder.Services.AddFeatureManagement();
             builder.Services.AddControllers();
 
             var app = builder.Build();
@@ -17,7 +32,7 @@ namespace Lab11
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
+            app.UseAzureAppConfiguration();
 
             app.MapControllers();
 
